da90be7584be5741261101d130eee49a10900111
Robertas <r.ankudovicius@gmail.com>
2020-09-28 21:07:00 +0300

    added map to map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const Map = ({ height, width }) => {
+  const [center, setCenter] = useState({ lat: 59.95, lng: 30.33 });
+  const [zoom, setZoom] = useState(11);
+
   return (
     <div
       style={{
@@ -8,7 +14,16 @@ const Map = ({ height, width }) => {
         width: width,
         background: 'purple',
         float: 'right',
-      }}></div>
+        height: '100vh',
+        width: '70%',
+      }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyC87zB-AkjcA1HQFBkM370neC_vj1Y2PMo' }}
+        defaultCenter={center}
+        defaultZoom={zoom}>
+        <AnyReactComponent lat={69.955413} lng={30.337844} text='My Marker' />
+      </GoogleMapReact>
+    </div>
   );
 };
 
